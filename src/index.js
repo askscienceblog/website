76527afb9a0b372ca868192d26618353c9636d49
Wang Chen <wangchensemail@gmail.com>
2025-12-29 20:09:21 +0800

    added support for r2 asset hosting

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 export default {
   async fetch(request, env) {
-    return new Response("Hello Worker!");
+    const req_url = new URL(request.url);
+
+    if (req_url.pathname.split("/")[1] == "static") {
+      const file = await env.STATIC.get(req_url.pathname.split("/").slice(2).join("/"));
+      if (file != null) {
+        const headers = new Headers()
+        file.writeHttpMetadata(headers)
+        return new Response(file.body, { headers });
+      }
+    }
+
+    const not_found_page = await (await env.ASSETS.fetch(new URL("/404.html", request.url))).text();
+    return new Response(not_found_page, { status: 404, headers: { "Content-Type": "text/html" } });
   },
 };
