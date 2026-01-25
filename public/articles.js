33bc7b04df47e085a646cb1e45e058e6c21a9747
Wang Chen <wangchensemail@gmail.com>
2026-01-25 09:47:47 +0800

    improved description and minification

--- public/articles.js
+++ public/articles.js
@@ -12,28 +12,30 @@ function toggleTOC(element) {
     on_off = !on_off
 }
 
-const headings = []
-for (const anchor of document.getElementById("TOC").querySelectorAll("a")) {
-    headings.push({ anchor, "heading": document.getElementById(anchor.getAttribute("href").slice(1)) })
-}
-
-const header_height = document.getElementById("header").getBoundingClientRect().bottom
-
-window.addEventListener("scroll", () => {
-    for (const [ind, obj] of headings.entries()) {
-        let h = obj.heading.getBoundingClientRect().y
-        let next_height;
-        try {
-            next_height = headings[ind + 1].heading.getBoundingClientRect().y
-        } catch {
-            next_height = Infinity
-        }
-        if (h < window.innerHeight && header_height + 4 < next_height) {
-            obj.anchor.setAttribute("class", "current")
-        } else if (h >= window.innerHeight) {
-            obj.anchor.setAttribute("class", "after")
-        } else {
-            obj.anchor.setAttribute("class", "before")
-        }
+(() => {
+    const headings = []
+    for (const anchor of document.getElementById("TOC").querySelectorAll("a")) {
+        headings.push({ anchor, "heading": document.getElementById(anchor.getAttribute("href").slice(1)) })
     }
-})
+
+    const header_height = document.getElementById("header").getBoundingClientRect().bottom
+
+    window.addEventListener("scroll", () => {
+        for (const [ind, obj] of headings.entries()) {
+            let h = obj.heading.getBoundingClientRect().y
+            let next_height;
+            try {
+                next_height = headings[ind + 1].heading.getBoundingClientRect().y
+            } catch {
+                next_height = Infinity
+            }
+            if (h < window.innerHeight && header_height + 4 < next_height) {
+                obj.anchor.setAttribute("class", "current")
+            } else if (h >= window.innerHeight) {
+                obj.anchor.setAttribute("class", "after")
+            } else {
+                obj.anchor.setAttribute("class", "before")
+            }
+        }
+    })
+})()
