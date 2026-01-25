d183bf578fb795dbad668107eb8aaf3c649efe35
Wang Chen <wangchensemail@gmail.com>
2026-01-25 18:13:18 +0800

    improved 404 page text wrapping

--- public/404.js
+++ public/404.js
@@ -2,11 +2,13 @@
     const div = document.createElement("div");
     let para = document.createElement('p');
     para.innerHTML = `The server could not find <a href="${window.location.href}">${window.location.href}</a>.`;
+    para.setAttribute("style", "text-wrap:balance")
     div.appendChild(para);
 
     para = document.createElement('p');
     para.innerHTML = `This link might be temporarily broken, or no longer exist permanently.
 Please head back to our <a href="/">homepage</a>.`;
+    para.setAttribute("style", "text-wrap:balance")
     div.appendChild(para);
 
     document.getElementById("notfound-msg").replaceWith(div)
