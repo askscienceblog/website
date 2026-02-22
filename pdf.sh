0957a88aa72c8269da8f5f01b187b7318478fd71
Wang Chen <wangchensemail@gmail.com>
2026-02-22 15:21:24 +0800

    added way to bypass pdf gen through PDF_DEV_MODE env var

--- pdf.sh
+++ pdf.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-if [ -f "$1" ]; then
+if [ -z "$PDF_DEV_MODE" ]; then
   echo "Processing file: $1"
-  jq ".markdown" $1 -r | pandoc -f markdown -t context --citeproc --csl=apa.csl --template=pandoc.tex > main.tex
+else
+  echo "PDF_DEV_MODE is set, skipping pdf generation and upload"
+  exit 0
+fi
+
+if [ -f "$1" ]; then
+  jq ".markdown" "$1" -r | pandoc -f markdown -t context --citeproc --csl=apa.csl --template=pandoc.tex > main.tex
   mkdir -p static
   grep -oP "(?<=[\(\[\{\"']/static/)(.*?)(?=[\)\]\}\"'])" main.tex | while IFS= read -r line; do
       echo "$line" | jq -rR @uri | sed "s/^/https:\/\/cms.ayrj.org\/static\//" | xargs -P 30 -I {} curl -o "static/$line" {}
   done
   sed -i "s/\/static\//.\/static\//g" main.tex
   context main.tex
-  pdfname=$(basename $1 .json)
+  pdfname=$(basename "$1" .json)
   npx wrangler r2 object put "static-assets/$pdfname.pdf" --file=main.pdf --remote
   echo "Uploaded $pdfname.pdf"
 fi
