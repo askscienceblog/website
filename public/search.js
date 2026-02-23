2886eca51a94df150bad45d64a5dbe00c11f8e48
Wang Chen <wangchensemail@gmail.com>
2026-02-23 19:33:22 +0800

    added search term highlighting

--- public/search.js
+++ public/search.js
@@ -18,7 +18,7 @@
     const query = document.getElementById("search-q")
     const list = document.getElementsByClassName("article-list").item(0)
 
-    query.addEventListener("input", (_) => {
+    const f = (_) => {
         let input;
         if (query.value) {
             input = query.value;
@@ -31,15 +31,39 @@
             .forEach((obj) => {
                 const li = document.createElement("li")
                 li.innerHTML = obj.html
+                const walker = document.createTreeWalker(li, NodeFilter.SHOW_TEXT, (node) => {
+                    const segments = Array.from(segmenter.segment(node.nodeValue))
+                        .map((obj) => obj.segment)
+                        .filter((txt) => txt.search(/[\p{L}\p{N}\p{S}]/v) >= 0)
+                        .map((txt) => txt.normalize("NFKC").trim().toLocaleLowerCase(LOCALES)) //recreate tokenization of searcher
+                    if (segments.includes(term)) {
+                        // console.log(segments)
+                        return NodeFilter.FILTER_ACCEPT
+                    } else {
+                        return NodeFilter.FILTER_REJECT
+                    }
+                })
+
+                for (term of obj.terms) {
+                    walker.currentNode = walker.root
+
+                    let node = walker.nextNode()
+                    const nodes = []
+                    while (node) {
+                        nodes.push(node)
+                        node = walker.nextNode()
+                    }
+
+                    for (n of nodes) {
+                        const l = document.createElement("span")
+                        l.innerHTML = n.nodeValue.replace(new RegExp(`(${term})`, "gi"), "<mark>$1</mark>")
+                        n.replaceWith(l)
+                    }
+                }
+
                 list.appendChild(li)
             })
-    })
-
-    searcher.search(MiniSearch.wildcard)
-        .sort((a, b) => a.score - b.score)
-        .forEach((obj) => {
-            const li = document.createElement("li")
-            li.innerHTML = obj.html
-            list.appendChild(li)
-        })
+    }
+    query.addEventListener("input", f)
+    f()
 })()
