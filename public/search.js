15de1573a39dbc52a778e8f7c9004402e8906c4d
Wang Chen <wangchensemail@gmail.com>
2026-02-25 14:42:03 +0800

    improved search ui with stable open windows and dom manipulation

--- public/search.js
+++ public/search.js
@@ -1,5 +1,6 @@
 (async () => {
     const LOCALES = ["en-SG", "en-US"]
+    const SPACES_ONLY = /^[\p{M}\p{Z}\p{C}]*$/v
     const segmenter = new Intl.Segmenter(
         LOCALES,
         { "granularity": "word" }
@@ -17,6 +18,7 @@
 
     const query = document.getElementById("search-q")
     const list = document.getElementsByClassName("article-list").item(0)
+    const previousSelected = new Set();
 
     const f = (_) => {
         let input;
@@ -26,41 +28,79 @@
             input = MiniSearch.wildcard
         }
         list.innerHTML = ""
+
+        console.log(previousSelected)
         searcher.search(input)
             .sort((a, b) => a.score - b.score)
             .forEach((obj) => {
                 const li = document.createElement("li")
                 li.innerHTML = obj.html
-                const walker = document.createTreeWalker(li, NodeFilter.SHOW_TEXT, (node) => {
-                    const segments = Array.from(segmenter.segment(node.nodeValue))
-                        .map((obj) => obj.segment)
-                        .filter((txt) => txt.search(/[\p{L}\p{N}\p{S}]/v) >= 0)
-                        .map((txt) => txt.normalize("NFKC").trim().toLocaleLowerCase(LOCALES)) //recreate tokenization of searcher
-                    if (segments.includes(term)) {
-                        // console.log(segments)
-                        return NodeFilter.FILTER_ACCEPT
-                    } else {
-                        return NodeFilter.FILTER_REJECT
-                    }
-                })
+                const walker = document.createTreeWalker(li, NodeFilter.SHOW_TEXT)
+                let node = walker.nextNode()
+                const mactchedTerms = new Set(obj.terms)
 
-                for (term of obj.terms) {
-                    walker.currentNode = walker.root
-
-                    let node = walker.nextNode()
-                    const nodes = []
-                    while (node) {
-                        nodes.push(node)
-                        node = walker.nextNode()
+                let nodes = []
+                while (node) {
+                    const k = Array.from(segmenter.segment(node.nodeValue)) //recreate segmentation process
+                        .filter((segment) =>
+                            mactchedTerms.has(segment.segment.normalize("NFKC").trim().toLocaleLowerCase(LOCALES)))
+                        .map((c) => [c.index, c.index + c.segment.length])
+                    if (k.length > 0) {
+                        nodes.push({ node, "values": k })
                     }
-
-                    for (n of nodes) {
-                        const l = document.createElement("span")
-                        l.innerHTML = n.nodeValue.replace(new RegExp(`(${term})`, "gi"), "<mark>$1</mark>")
-                        n.replaceWith(l)
-                    }
+                    node = walker.nextNode()
                 }
 
+                for (const thisNode of nodes) {
+                    thisNode.node.parentNode.insertBefore(
+                        document.createTextNode(
+                            thisNode.node.nodeValue.slice(0, thisNode.values[0][0])
+                        ),
+                        thisNode.node
+                    );
+                    const text = thisNode.node.nodeValue;
+                    let prevNode = document.createElement("mark");
+                    prevNode.innerText = text.slice(thisNode.values[0][0], thisNode.values[0][1])
+                    thisNode.node.parentNode.insertBefore(prevNode, thisNode.node)
+
+
+                    for (let i = 1; i < thisNode.values.length; i++) {
+                        const currText = text.slice(thisNode.values[i - 1][1], thisNode.values[i][0])
+                        if (SPACES_ONLY.test(currText)) {
+                            prevNode.innerText += currText + text.slice(thisNode.values[i][0], thisNode.values[i][1])
+                        } else {
+                            thisNode.node.parentNode.insertBefore(
+                                document.createTextNode(currText), thisNode.node
+                            )
+                            prevNode = document.createElement("mark")
+                            prevNode.innerText = text.slice(thisNode.values[i][0], thisNode.values[i][1])
+                            thisNode.node.parentNode.insertBefore(prevNode, thisNode.node)
+                        }
+                    }
+
+                    thisNode.node.parentNode.insertBefore(
+                        document.createTextNode(
+                            text.slice(thisNode.values[thisNode.values.length - 1][1])
+                        ),
+                        thisNode.node
+                    )
+                    thisNode.node.parentNode.removeChild(thisNode.node)
+                }
+
+                li.firstElementChild.setAttribute("doc-id", obj.id)
+                li.firstElementChild.addEventListener(
+                    "toggle",
+                    (e) => {
+                        if (e.newState === "open") {
+                            previousSelected.add(li.firstElementChild.getAttribute("doc-id"))
+                        } else {
+                            previousSelected.delete(li.firstElementChild.getAttribute("doc-id"))
+                        }
+                    }
+                )
+                if (previousSelected.has(obj.id.toString())) {
+                    li.firstElementChild.setAttribute("open", "")
+                }
                 list.appendChild(li)
             })
     }
